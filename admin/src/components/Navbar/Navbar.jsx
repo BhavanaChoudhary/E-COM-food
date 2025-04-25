360cfe7c0f6216820685f10620c25a4012a29dfc
bhavana <bhavanabc017@gmail.com>
2025-04-26 00:49:27 +0530

    .

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import Login from '../../pages/Login/Login'
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
         <img className='logo' src={assets.logo} alt="" />
         <img className='profile' src={assets.profile_image} alt="" />
-        
+        <NavLink to="/login" className="login-link" style={{ marginLeft: 'auto', color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>
+          Admin Login
+        </NavLink>
     </div>
   )
 }
