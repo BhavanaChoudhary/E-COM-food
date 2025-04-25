607e98f8d46d39fd04d57c612b2bf7389bfae5da
bhavana <bhavanabc017@gmail.com>
2025-04-26 01:58:36 +0530

    completed succesfully

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);
+  }, []);
+
   return (
     <div className='navbar'>
         <img className='logo' src={assets.logo} alt="" />
-        <Link to="/login" className="login-button">Login</Link>
+        {!isLoggedIn && <Link to="/login" className="login-button">Login</Link>}
         <img className='profile' src={assets.profile_image} alt="" />
-       
     </div>
   )
 }
